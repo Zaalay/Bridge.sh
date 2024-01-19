8e0e38ea8b7c69ea24d0d28a1be6b999a60df7e1
Muhammad Rivan <rivanfebrian123@gmail.com>
2024-01-19 10:17:02 +0000

    update

--- modules/linux.sh
+++ modules/linux.sh
@@ -1,6 +1,34 @@
 #!/usr/bin/env bash
 # Part of Bridge.sh, MIT-licensed
-# Copyright (c) 2022 Zaalay Studio, Muhammad Rivan
+# Copyright (c) 2024 Zaalay Studio, Muhammad Rivan
+
+sed() {
+  local cmd=("${BRIDGE_BINDIR}/sed")
+
+  while [[ $# -gt 0 ]]; do
+    case "${1}" in
+      -i)
+        if [[ $# -ge 3 && ${2} == '-e' ]]; then
+          cmd+=('-i' "${3}")
+          shift 3
+        elif [[ $# -ge 4 && ${2} == '' && ${3} == '-e' ]]; then
+          cmd+=('-i' "${4}")
+          shift 4
+        else
+          cmd+=("${1}")
+          shift
+        fi
+        ;;
+      *)
+        cmd+=("${1}")
+        shift
+        ;;
+    esac
+  done
+
+  echo "${cmd[@]}"
+  "${cmd[@]}"
+}
 
 if ! (return 0 2> /dev/null); then
   source "$(dirname "$(dirname "${0}")")/modules/core.sh" "full"
